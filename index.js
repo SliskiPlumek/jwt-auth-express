ebb94c820583b9031a1167b1ff9687a0a1432029
Plumek <hsliwinski49@gmail.com>
2023-06-27 11:22:59 +0200

    commit

--- index.js
+++ index.js
@@ -30,33 +30,4 @@ function authenticateToken(secretName) {
   };
 }
 
-// function authenticateToken(req, res, next) {
-//   const authHeader = req.get("Authorization");
-
-//   if (!authHeader) {
-//     const error = new Error("Not authenticated.");
-//     error.statusCode = 401;
-//     throw error;
-//   }
-
-//   const token = authHeader.split(" ")[1];
-//   let decodedToken;
-
-//   try {
-//     decodedToken = jwt.verify(token, "somesupersecretsecret");
-//   } catch (err) {
-//     err.statusCode = 500;
-//     throw err;
-//   }
-
-//   if (!decodedToken) {
-//     const error = new Error("Not authenticated.");
-//     error.statusCode = 401;
-//     throw error;
-//   }
-  
-//   req.userId = decodedToken.userId;
-//   next();
-// }
-
 module.exports = authenticateToken;
