98820c010bf1b7e52c177d96c5bfbb7a48a5774e
Plumek <hsliwinski49@gmail.com>
2023-06-28 23:53:15 +0200

    bug fix1

--- index.js
+++ index.js
@@ -5,27 +5,29 @@ const app = express();
 
 function authenticateToken(secretName) {
   return function (req, res, next) {
-    const authHeader = req.get('Authorization');
-    
+    const authHeader = req.get("Authorization");
+
     if (!authHeader) {
-      const error = new Error('Not authenticated.');
-      error.statusCode = 401;
-      throw error;
+      req.isAuth = false;
+      return next();
     }
-    const token = authHeader.split(' ')[1];
+
+    const token = authHeader.split(" ")[1];
     let decodedToken;
+
     try {
       decodedToken = jwt.verify(token, secretName);
     } catch (err) {
       err.statusCode = 500;
-      throw err;
+      req.isAuth = false
+      return next()
     }
     if (!decodedToken) {
-      const error = new Error('Not authenticated.');
-      error.statusCode = 401;
-      throw error;
+      req.isAuth = false
+      return next()
     }
     req.userId = decodedToken.userId;
+    req.isAuth = true
     next();
   };
 }
